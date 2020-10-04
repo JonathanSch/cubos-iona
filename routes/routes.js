8ff6c318c59421f3128d22f8b9ed6f00ad26aeb2
JonathanSch <jonathansch7@gmail.com>
2020-10-04 16:26:13 -0500

    allow

--- routes/routes.js
+++ routes/routes.js
@@ -3,6 +3,5 @@ const router = express.Router();
 const rutasCubos = require('./cuboRoutes')
 
 router.use('/cubos', rutasCubos);
-router.use("Access-Control-Allow-Origin", '*')
 
 module.exports = router;
