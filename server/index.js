f087b4b187cf9e83d07547808616e7798c3106fd
JonathanSch <jonathansch7@gmail.com>
2020-10-04 16:46:29 -0500

    allow

--- server/index.js
+++ server/index.js
@@ -1,17 +1,13 @@
 const express = require('express')
+const cors = require('cors')
 const app = express()
+app.use(cors)
 const PORT = process.env.PORT || 3000
 app.use(express.json())
 const routes = require('../routes/routes')
 
-app.use('/api',routes);
-app.use(function(req, res, next) {
-    req.header("Access-Control-Allow-Origin", '*');
-    req.header("Access-Control-Allow-Credentials", true);
-    req.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-    req.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
-    next();
-});
+app.use('/api', routes);
+
 
 app.get('/', (req, res) => {
     res.status(200).send('Todo ok')
