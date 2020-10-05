6322a6508ac72ec8d1d1eb787671a0f867479a1a
JonathanSch <jonathansch7@gmail.com>
2020-10-05 09:09:40 -0500

    hash added

--- controllers/UsuarioControllers.js
+++ controllers/UsuarioControllers.js
@@ -4,8 +4,8 @@ module.exports = {
     crearUsuario : async(req,res) => {
         const usuario = req.body
         try {
-            services.crearUsuario(req.body)
-            res.send(usuario).status(201)
+            const usuariocreado = await services.crearUsuario(usuario)
+            res.send(usuariocreado).status(201)
         } catch (error) {
             res.send(error).status(400)
         }
--- models/Usuario.js
+++ models/Usuario.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-
+const bcrypt = require('bcrypt')
 const UsuarioSchema = new mongoose.Schema({
     nombreUsuario: {
         type: String,
@@ -10,6 +10,21 @@ const UsuarioSchema = new mongoose.Schema({
         type: String,
         required: true
     }
+})
+UsuarioSchema.pre('save', function (next) {
+    const usuario = this;
+    if (!usuario.isModified('password')) return next();
+
+    bcrypt.genSalt(10, (err, salt) => {
+        if (err) return next(err);
+
+        bcrypt.hash(usuario.password, salt, (error, hash) => {
+            if (error) return next(error);
+
+            usuario.password = hash
+            return next();
+        })
+    })
 })
 
 const Usuario = mongoose.model('Usuario', UsuarioSchema)
